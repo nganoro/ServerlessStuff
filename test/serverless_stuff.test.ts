0e78c73e5d5dc04a0c15fcb1a5849429ca52c3e5
nganoro <nganoro1@umbc.edu>
2022-10-25 17:36:01 -0400

    First CDK commit

---
+++ test/serverless_stuff.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ServerlessStuff from '../lib/serverless_stuff-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/serverless_stuff-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ServerlessStuff.ServerlessStuffStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
