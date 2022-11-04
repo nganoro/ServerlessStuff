decd2e24591009a56ee919a2f019e5e52ef7bcaf
nganoro <nganoro1@umbc.edu>
2022-11-04 12:19:44 -0400

    First CDK commit

--- lib/serverless_stuff-stack.ts
+++ lib/serverless_stuff-stack.ts
@@ -1,16 +1,22 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import {Code, Function, Runtime} from 'aws-cdk-lib/aws-lambda';
+import { join } from 'path';
+import { Bucket } from "aws-cdk-lib/aws-s3";
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class ServerlessStuffStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new Bucket(this, 'AWSBuilderBucket4Nate', {})
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ServerlessStuffQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const handler = new Function(this, 'Hello-Lambda', {
+      runtime: Runtime.NODEJS_14_X,
+      memorySize: 512,
+      handler: 'app.handler',
+      code: Code.fromAsset(join(__dirname, '../lambdas'))
+    })
   }
 }
