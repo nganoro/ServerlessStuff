6b1df6c5ddf14ecf7ace54acea45f0084ab76e3f
Nate Ganoro <nganoro@amazon.com>
2022-11-24 22:26:36 -0500

    dynamically name profile pic names

--- services/lambdas/s3URLambda.ts
+++ services/lambdas/s3URLambda.ts
@@ -1,6 +1,7 @@
 // import * as AWS from 'aws-sdk';
 import {APIGatewayProxyEventV2, APIGatewayProxyResult, Context} from 'aws-lambda';
 const AWS = require("aws-sdk");
+const PRIMARY_KEY = 'fileName';
 
 export async function handler(event: APIGatewayProxyEventV2, context: Context): Promise<APIGatewayProxyResult> {
     const type = event.pathParameters!['type'];
@@ -23,33 +24,33 @@ export async function handler(event: APIGatewayProxyEventV2, context: Context): 
     if (type == 'post') {
 
         const bucketName = 'serverlessstuffstack-imagesbucket1e86afb2-acjky0zhzn6z';
-        // const keyValue = event.queryStringParameters![PRIMARY_KEY] + '.jpeg';
-        var key = 'videos' + '/' + 'SampleVideo_1280x720_1mb.jpeg'; //im using a hardcoded filename, but get the file name dynamically that you'll upload to the s3 bucket
+        const keyValue = event.queryStringParameters![PRIMARY_KEY] + '.jpeg';
+        // var key = 'videos' + '/' + 'SampleVideo_1280x720_1mb.jpeg'; //im using a hardcoded filename, but get the file name dynamically that you'll upload to the s3 bucket
         const signedUrlExpireSeconds = 60 * 5;
 
         const url = s3.getSignedUrl('putObject', {
             Bucket: bucketName,
-            Key: key,
+            Key: keyValue,
             Expires: signedUrlExpireSeconds,
             ContentType: 'image/jpeg'
         });
 
-        result.body =  JSON.stringify({filename: key, presigned_url: url});
+        result.body =  JSON.stringify({filename: keyValue, presigned_url: url});
 
     } else if(type == 'get'){
 
         const bucketName = 'serverlessstuffstack-imagesbucket1e86afb2-acjky0zhzn6z';
-        // const keyValue = event.queryStringParameters![PRIMARY_KEY] + '.jpeg';
-        var key = 'videos' + '/' + 'SampleVideo_1280x720_1mb.jpeg'; //im using a hardcoded filename, but get the file name dynamically that you'll upload to the s3 bucket
-        const signedUrlExpireSeconds = 60 * 5;
+        const keyValue = event.queryStringParameters![PRIMARY_KEY] + '.jpeg';
+        // var key = 'videos' + '/' + 'SampleVideo_1280x720_1mb.jpeg'; //im using a hardcoded filename, but get the file name dynamically that you'll upload to the s3 bucket
+        const signedUrlExpireSeconds = 60 * 5
 
         const url = s3.getSignedUrl('getObject', {
             Bucket: bucketName,
-            Key: key,
+            Key: keyValue,
             Expires: signedUrlExpireSeconds,
         });
 
-        result.body =  JSON.stringify({filename: key, presigned_url: url});
+        result.body =  JSON.stringify({filename: keyValue, presigned_url: url});
 
     }
 
