7ca5dd28dddd7cfc192bfae975f21d42db214c82
Nate Ganoro <nganoro@amazon.com>
2022-12-13 22:52:00 -0500

    cloudfront and new bucket added

--- lib/serverless_stuff-stack.ts
+++ lib/serverless_stuff-stack.ts
@@ -1,6 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import {Construct} from 'constructs';
-import { Bucket, HttpMethods } from "aws-cdk-lib/aws-s3";
+import {Bucket, HttpMethods} from "aws-cdk-lib/aws-s3";
 import {Authorization} from "../Infrastructure/Authorization";
 import {AuthorizationType, Cors, LambdaIntegration, MethodOptions, RestApi} from "aws-cdk-lib/aws-apigateway";
 import {DynamoDb} from "../Infrastructure/DynamoDb";
@@ -8,6 +8,11 @@ import * as iam from 'aws-cdk-lib/aws-iam';
 import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
 import {Runtime} from "aws-cdk-lib/aws-lambda";
 import * as path from "path";
+import * as CloudFront from 'aws-cdk-lib/aws-cloudfront';
+import {CloudFrontAllowedMethods} from 'aws-cdk-lib/aws-cloudfront';
+import {Duration} from "aws-cdk-lib";
+
+const site_url = 'ab3sitebucket-nganoro'
 
 export class ServerlessStuffStack extends cdk.Stack {
 
@@ -120,6 +125,54 @@ export class ServerlessStuffStack extends cdk.Stack {
     postProfileLambdaResource.addMethod('GET', this.ab3FinalTable.profileReadLambdaIntegeration, optionsWithAuthorizer);
     postProfileLambdaResource.addMethod('PUT', this.ab3FinalTable.profileUpdateLambdaIntegeration, optionsWithAuthorizer);
 
-    }
+    //website hosting bucket
+    const ab3SiteBucket_Natnael = new Bucket(
+        this, 'ab3SiteBucket_Natnael', {
+            publicReadAccess: false,
+            cors: [{
+                allowedMethods: [
+                    HttpMethods.HEAD,
+                    HttpMethods.GET,
+                    HttpMethods.PUT,
+                    HttpMethods.DELETE
+                ],
+                allowedOrigins: ['*'],
+                allowedHeaders: ['*']
+            }],
+            bucketName: site_url,
+            websiteIndexDocument: 'index.html'
+        }
+    )
 
+
+    //Cloudfront certificates
+    // const certificationArn = new acm.Dns
+
+    // CloudFront distributions
+    const cfIndentity = new CloudFront.OriginAccessIdentity(this, 'cfId');
+    ab3SiteBucket_Natnael.addToResourcePolicy(new iam.PolicyStatement({
+        actions: ["s3:GetObject"],
+        resources: [ab3SiteBucket_Natnael.arnForObjects("*")],
+        principals: [new iam.CanonicalUserPrincipal(cfIndentity.cloudFrontOriginAccessIdentityS3CanonicalUserId)]
+    }));
+    const distribution = new CloudFront.CloudFrontWebDistribution(this, 'ab3sitedistributions', {
+        originConfigs: [
+            {
+                s3OriginSource: {
+                    s3BucketSource: ab3SiteBucket_Natnael,
+                    originAccessIdentity: cfIndentity
+                },
+                behaviors: [{
+                    isDefaultBehavior: true,
+                    allowedMethods: CloudFrontAllowedMethods.GET_HEAD,
+                    defaultTtl: Duration.days(0),
+                    compress: true,
+                }],
+            }
+        ],
+        // viewerCertificate: CloudFront.ViewerCertificate.fromCloudFrontDefaultCertificate(
+        //     'd111111abcdef8.cloudfront.net'
+        // ),
+    })
+    }
 }
